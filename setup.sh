68096075f8e30d5693b2f42d5328ac4f376b289c
Martstol <martstol@gmail.com>
2018-04-28 18:32:56 +0200

    Added setup script

---
+++ setup.sh
@@ -0,0 +1,8 @@
+apt-get install git
+apt-get install curl
+apt-get install fonts-firacode
+apt-get install neovim
+apt-get install tmux
+apt-get install gnome-shell-extensions
+apt-get install gnome-shell-extension-weather
+
